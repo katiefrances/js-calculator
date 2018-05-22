cd8ec0defd59b6a30fcba7746015c1a301f0e685
Katie Frances <katiebeeisdead@gmail.com>
2018-05-22 15:58:10 +1000

    Add operator functions

--- calc.js
+++ calc.js
@@ -4,7 +4,6 @@
 // Add buttons and forms for user input
 // Actually make it work
 
-
 class Calculate {
     constructor (a,b,operator) {
         this.a = a;
@@ -17,49 +16,44 @@ class Calculate {
         console.log(answer);
     }
 
+    subtract() {
+        answer = this.a - this.b;
+        console.log(answer);
+    }
+
+    divide() {
+        answer = this.a / this.b;
+        console.log(answer);
+    }
+
+    multiply() {
+        answer = this.a * this.b;
+        console.log(answer);
+    }
+
 }
 
 function add() {
     a = parseInt(document.getElementById("number1").value);
     b = parseInt(document.getElementById("number2").value);
-    console.log(a+b);
+    console.log(a + b);
 }
 
+function subtract() {
+    a = parseInt(document.getElementById("number1").value);
+    b = parseInt(document.getElementById("number2").value);
+    console.log(a - b);
+}
 
+function divide() {
+    a = parseInt(document.getElementById("number1").value);
+    b = parseInt(document.getElementById("number2").value);
+    console.log(a / b);
+}
 
+function multiply() {
+    a = parseInt(document.getElementById("number1").value);
+    b = parseInt(document.getElementById("number2").value);
+    console.log(a * b);
+}
 
-
-
-// functions for operators
-// let add = () => {alert(parseInt(document.getElementById("number1").value)) + parseInt(document.getElementById("number2").value)};
-// let add = () => {alert(parseInt(document.getElementById("number1").value) + parseInt(document.getElementById("number2").value))};
-
-let subtract = (c,d) => {return c - d};
-let divide = (e,f) => {return e / f };
-let multiply = (g,h) => {return g * h};
-
-
-// operators, maybe put in a function that calls all the functions at a later point?
-// function calculate() {
-//     // something here about calling functions based off the button clicked
-//     let operator = document.getElementById("input").value;
-//     let myAnswer;
-
-//     if (operator == "+") {
-//         myAnswer = add(firstNum,secondNum);
-//     } else if (operator == "-") {
-//         myAnswer = subtract(firstNum,secondNum);
-//     } else if (operator == "/") {
-//         myAnswer = divide(firstNum,secondNum); 
-//     } else if (operator=="*") {
-//         myAnswer = multiply(firstNum,secondNum);
-//     } else {
-//         myAnswer = "You're an idiot";
-//     }
-//     console.log(myAnswer);
-// }
-
-// function output() {
-//     return calculate();
-//     // a bad attempt at some kind of equal function ?
-// }
